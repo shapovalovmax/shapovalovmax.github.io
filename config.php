4390365706dac813355608d8f8f5f0ab758a77c9
pd-maksymshapovalov <maksym.shapovalov@pinedev.eu>
2023-12-11 14:57:27 +0200

    start files

--- config.php
+++ config.php
@@ -1,3 +1,5 @@
 <?php
 
 define('TOKEN','6807989948:AAG-GQbh0FG3ah9Nkmv-Iki-I31ZUWQV7EM');
+
+https://api.telegram.org/bot6807989948:AAG-GQbh0FG3ah9Nkmv-Iki-I31ZUWQV7EM/getMe
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+echo 'hello';
+die;
+
 require_once __DIR__ . 'vendor/autoload.php';
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/funcs.php';
